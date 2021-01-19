41e13c10614131e250306b79c45e066f3aa5d41f
Juani Balaguer <balaguerjuani@gmail.com>
2021-01-19 18:15:10 -0300

    Prueba de chart.js

---
+++ PruebaChartJS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaChartJS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
